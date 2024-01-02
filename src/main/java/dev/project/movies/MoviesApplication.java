dcbd2ffcb665c61451aff7ad8034e9a820bc7259
Ankit Dani <ankitdani1997@gmail.com>
2024-01-01 17:17:11 -0800

    Added swagger for api documentation

--- src/main/java/dev/project/movies/MoviesApplication.java
+++ src/main/java/dev/project/movies/MoviesApplication.java
@@ -2,15 +2,27 @@ package dev.project.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-
+@EnableSwagger2
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("dev.project.movies"))
+				.build();
 	}
 
 }
