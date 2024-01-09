1b063e9fa20bc4557693ffccdcfed76714878409
Ankit Dani <ankitdani1997@gmail.com>
2024-01-08 21:43:10 -0800

    Removed unused libraries from MoviesApplication file

--- src/main/java/dev/project/movies/MoviesApplication.java
+++ src/main/java/dev/project/movies/MoviesApplication.java
@@ -2,9 +2,6 @@ package dev.project.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
